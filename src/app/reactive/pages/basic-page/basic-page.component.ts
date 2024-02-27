fc9fdf82fe3b6d6f3afa477906ac8a311d5ceae2
Daniel Borrome <dborrome@gmail.com>
2024-02-27 17:46:29 -0400

    Corregir mensajes de error en formulario de página básica

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 1000,
+  inStorage: 10,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
   //   price: new FormControl(0),
@@ -20,9 +26,45 @@ export class BasicPageComponent {
 
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFielError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es obligatorio';
+        case 'minlength':
+          return `Este campo debe ser al menos ${errors['minlength'].requiredLength} caracteres `;
+        case 'min':
+          return 'Este campo debe ser 0 o mayor';
+        default:
+          return 'Error no identificado';
+      }
+    }
+
+    return null;
+  }
+
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
