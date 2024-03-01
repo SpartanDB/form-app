4ce26991bb331671e08ac35422349581919ee74f
Daniel Borrome <dborrome@gmail.com>
2024-03-01 11:05:22 -0400

    Actualizar validaciones de campos en la página de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,34 +1,69 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from '../../../shared/validators/validators';
+
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
-  public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: [
-      '',
-      [Validators.required, Validators.email, Validators.pattern(customValidators.emailPattern) ],
-    ],
-    username: ['', [Validators.required, customValidators.cantBeStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
-  });
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        // [new EmailValidatorService() ],
+        [this.emailValidatorService],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFielOneEqualToFieldTwo(
+          'password',
+          'password2'
+        ),
+      ],
+    }
+  );
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService
+  ) {}
 
-  isValidField(field: string) {
-    // TODO: obtener validación desde un servicio o un helper
+  isValidField(field: string): boolean | null {
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
+    if (this.myForm.pending) return;
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
 
     console.log(this.myForm.value);
+    this.myForm.reset();
   }
 }
